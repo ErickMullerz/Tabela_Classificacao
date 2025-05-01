6f694d49786a5b11f1f75b392488f9fe74bc1b22
Müller <125224052+ErickMullerz@users.noreply.github.com>
2025-05-01 17:10:00 -0300

    Delete Classificação_python directory

--- Classificação_python/models.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def get_classificacao():
-    conn = sqlite3.connect('classificacao.db')
-    cur = conn.cursor()
-    cur.execute("SELECT time, pontos FROM classificacao ORDER BY pontos DESC")
-    dados = cur.fetchall()
-    conn.close()
-    return dados
