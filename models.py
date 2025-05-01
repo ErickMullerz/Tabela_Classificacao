7b6477d6d67b1ac4727fe57126142a4af63c7b33
Müller <125224052+ErickMullerz@users.noreply.github.com>
2025-05-01 17:17:25 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,5 @@
 import sqlite3
 
 def get_classificacao():
-    conn = sqlite3.connect('classificacao.db')
-    cur = conn.cursor()
-    cur.execute("SELECT time, pontos FROM classificacao ORDER BY pontos DESC")
-    dados = cur.fetchall()
-    conn.close()
+    dados = ["a"]
     return dados
