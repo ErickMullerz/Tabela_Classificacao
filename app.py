d56fee138a41cdd803f71aab770c6baa04f3376d
Müller <125224052+ErickMullerz@users.noreply.github.com>
2025-05-01 17:23:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
-from models import getClassificacao
+from flask import Flask, request, jsonify
+from typing import List, Dict
 
-app = Flask(name)
+app = Flask(__name__)
 
-@app.rout('/')
-def home():
-    tabela = classifica()
-    return render_template('classificacao.html',tabela = tabela)
+@app.route('/matches', methods=['POST'])
+def create_match():
+    try:
+        match_data = request.get_json()
+        if not all(key in match_data for key in ('id', 'group', 'homePlayer', 'homeClub', 'awayPlayer', 'awayClub', 'homeScore', 'awayScore', 'datetime')):
+            return jsonify({"error": "Faltando dados obrigatórios"}), 400
+
+        return jsonify(match_data), 201
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
